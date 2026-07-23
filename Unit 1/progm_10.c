0edddd15f1743581f44465bd88d9b8daf053d049
paida30 <paidamapurisa28@gmail.com>
2026-07-23 12:54:24 +0530

    Add files via upload

---
+++ Unit 1/progm_10.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+    int a[5], i, j, temp;
+
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+
+    for(i = 0; i < 4; i++)
+    {
+        for(j = i + 1; j < 5; j++)
+        {
+            if(a[i] < a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("\nElements in descending order:\n");
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+
+}
